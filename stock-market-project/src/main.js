b9a6dfe6da583e5ad2f0a9de5f32d2882e7c9ab6
pavloskii <pavlo.igor@gmail.com>
2018-04-26 21:44:12 +0200

    Done

--- stock-market-project/src/main.js
+++ stock-market-project/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App'
 import router from './router'
 import store from './store/store'
 
 Vue.config.productionTip = false
+
+Vue.use(VueResource);
+Vue.http.options.root = 'https://vuejs-stock-trader-3d1e6.firebaseio.com/';
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
--- stock-market-project/src/store/store.js
+++ stock-market-project/src/store/store.js
@@ -4,10 +4,13 @@ import Vuex from 'vuex';
 import stocks from './modules/stocks';
 import portfolio from './modules/portfolio';
 
+import * as actions from './actions';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules:{
+    actions: actions,
+    modules: {
         stocks,
         portfolio
     }
